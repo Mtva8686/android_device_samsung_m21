2b801bd4507ab1a0c59e387093fb5a12cd8c9a82
Mtva8686 <mtva8686@gmail.com>
2026-05-16 23:35:05 +0700

    Update and rename lineage_m21.mk to evox_m21.mk

--- evox_m21.mk
+++ evox_m21.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/samsung/m21/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evox/config/common_full_phone.mk)
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
@@ -32,7 +32,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 	BuildFingerprint="samsung/m21nsxx/m21:12/SP1A.210812.016/M215FXXU3CWB1:user/release-keys"
 
 ## Device configuration
-PRODUCT_NAME := lineage_m21
+PRODUCT_NAME := evox_m21
 PRODUCT_DEVICE := m21
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M215F
